a9fd7e83800e2d1066adda7ae34e77b974f4699c
Sree <sreepriya5155@gmail.com>
2022-12-05 09:30:27 +0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgModule  } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'exam';
+  arr: persondetails[]=[
+    // { rollnumber:'REG101', name:'Ajith',place:'Kannur',contact:9598321589},
+    // { rollnumber:'REG102', name:'Mariya',place:'Kozhikkode',contact:8751348712},
+  ]
+  registerClicked(e:any){
+    e.preventDefault();
+    this.lastRollNumber++;
+    this.arr.push({rollnumber:'REG'+this.lastRollNumber, name:this.name, place:this.place, contact:this.contact});
+  }
+  clearClicked()
+  {
+   this.arr=[];
+  }
+  removeClicked(rollnumber:any){
+     this.arr.findIndex(x=>x.rollnumber==rollnumber);
+    //  this.arr.splice(index, 1);
+  }
+
+  submitUser(userFrm:any){
+     console.log(userFrm);
+
+  }
+  
+  lastRollNumber:number=100;
+
+  name:string="";
+  place:string="";
+  contact:number=0;
+
+
+  
 }
+export interface persondetails {
+  rollnumber:string,
+  name:string,
+  place:string,
+  contact:number,
+ 
+}
+
+
